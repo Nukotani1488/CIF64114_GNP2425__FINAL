e58144be8e8437ffa078a0ca2b94d7b86e0f0870
nukotani <fatihsatrio121@gmail.com>
2025-05-15 14:16:50 +0700

    added temporary views for credential pages

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,28 +4,48 @@
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
 <!--[if gt IE 8]>      <html class="no-js"> <!--<![endif]-->
 <html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>index</title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="">
-    </head>
-    <body>
-        <!--[if lt IE 7]>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="{{ asset('css/global.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/dashboard.css') }}">
+    <style>
+        input {
+            display: block;
+            margin: 10px auto;
+        }
+    </style>
+</head>
+
+<body class="">
+    <!--[if lt IE 7]>
             <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
-        <form action="{{ route('login.post') }}" method="POST">
-            <input type="text" name="name" placeholder="Enter your name">
-            <input type="password" name="password" placeholder="Enter your password">
-            <input type="submit" value="Submit">
-            @csrf
-        </form>
-        @if ($errors)
-            @foreach ($errors->all() as $error)
-                <p style="color: red;">{{ $error }}</p>
-            @endforeach
-        @endif
-    </body>
+    <div class="header">
+        <h1>Login</h1>
+    </div>
+    <div class="body">
+        <div class="content-card">
+            <form action="{{ route('login.post') }}" method="POST">
+                <input type="text" name="name" placeholder="Enter your username or email" required>
+                <input type="password" name="password" placeholder="Enter your password" required>
+                <input type="submit" value="Submit">
+                @csrf
+            </form>
+            <a>
+                <p>Belum punya akun? <a href="{{ route('register') }}">Daftar di sini</a></p>
+        </div>
+        <div class="content-card">
+            @if ($errors)
+                @foreach ($errors->all() as $error)
+                    <p style="color: red;">{{ $error }}</p>
+                @endforeach
+            @endif
+        </div>
+    </div>
+</body>
 </html>
